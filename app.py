e0678dfab844a9905528c0d4b05193f4a63d6891
YoonkyungKim <yoonkyungkim.nlp@gmail.com>
2020-09-30 17:19:44 -0400

    Encapsulate the run

--- app.py
+++ app.py
@@ -78,4 +78,5 @@ def postalSearch():
 
 
 # debug=True: reload everytime we change the code and save it
-app.run(debug=True, port=8000, host='0.0.0.0')
+if __name__ == "main":
+    app.run(debug=True, port=8000, host='0.0.0.0')
